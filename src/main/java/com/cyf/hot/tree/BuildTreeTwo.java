da447523d0e8f8f0959043ba6993c557cc442010
cyf <1029839308@qq.com>
2022-10-08 22:48:22 +0800

    106. 从中序与后序遍历序列构造二叉树

--- src/main/java/com/cyf/hot/tree/BuildTreeTwo.java
+++ src/main/java/com/cyf/hot/tree/BuildTreeTwo.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 题目：106. 从中序与后序遍历序列构造二叉树
+ * 给定两个整数数组 inorder 和 postorder ，其中 inorder 是二叉树的中序遍历， postorder 是同一棵树的后序遍历，请你构造并返回这颗 二叉树 。
+ *
  * @author 陈一锋
  * @date 2022/10/7 3:46 下午
  */
@@ -14,13 +17,12 @@ public class BuildTreeTwo {
     public static void main(String[] args) {
         //inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
         BuildTreeTwo bean = new BuildTreeTwo();
-//        TreeNode node = bean.buildTree(new int[]{9, 3, 15, 20, 7}, new int[]{9, 15, 7, 20, 3});
-//        System.out.println(node);
+        TreeNode node = bean.buildTree(new int[]{9, 3, 15, 20, 7}, new int[]{9, 15, 7, 20, 3});
+        System.out.println(node);
 
-        TreeNode node_01 = bean.buildTree(new int[]{1,2}, new int[]{2,1});
-        System.out.println(node_01);
+//        TreeNode node_01 = bean.buildTree(new int[]{1,2}, new int[]{2,1});
+//        System.out.println(node_01);
     }
-
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         if(inorder == null || inorder.length == 0){
             return null;
@@ -29,11 +31,11 @@ public class BuildTreeTwo {
         for(int i = 0; i<inorder.length;i++){
             map.put(inorder[i],i);
         }
-        return buildTree(postorder,map,0,postorder.length-1);
+        return buildTree(postorder,map,postorder.length-1,0,inorder.length - 1);
     }
 
-    public TreeNode buildTree(int[] postorder, Map<Integer,Integer> map, int postLeft, int postRight){
-        if(postLeft > postRight){
+    public TreeNode buildTree(int[] postorder, Map<Integer,Integer> map ,int postRight,int inLeft,int inRight){
+        if(inLeft > inRight){
             return null;
         }
 
@@ -42,9 +44,8 @@ public class BuildTreeTwo {
         int idx = map.get(midVal);
         TreeNode node = new TreeNode(midVal);
 
-        int leftSize = idx - postLeft - 1;
-        node.left = buildTree(postorder,map,postLeft,leftSize);
-        node.right = buildTree(postorder,map,leftSize + 1,postRight-1);
+        node.left = buildTree(postorder,map,postRight - (inRight - idx) -1, inLeft, idx -1);
+        node.right = buildTree(postorder,map,postRight-1,idx+1,inRight);
 
         return node;
     }
